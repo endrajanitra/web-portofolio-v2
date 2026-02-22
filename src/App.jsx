33313202b3fdb93abcae01c3cb07bc128bc4440a
Endra Janitra <janitraendra@gmail.com>
2026-02-22 13:54:02 +0700

    feat: tampilan tentang & proyek

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import DataImage from "./data";
+import {listTools} from "./data"
 
 function App() {
 
@@ -20,6 +21,45 @@ function App() {
         </div>
       </div>
       <img src={DataImage.HeroImage} alt="hero image" className="w-[500px] md:ml-auto" />
+    </div>
+
+    {/* tentang */}
+    <div className="tentang mt-32 py-10">
+      <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-lg">
+        <img src={DataImage.HeroImage} alt="image"  className="w-12 rounded-md mb-10 sm:hidden"/>
+        <p className="text-base/loose mb-10">Hi, perkenalkan saya Diky Prayoga, seorang Full Stack Web Developer dan Designer untuk UI/UX Design maupun Product Digital, Saya percaya bahwa desain dan fungsionalitas harus berjalan beriringan, 
+          sehingga setiap proyek yang saya kembangkan tidak hanya terlihat menarik tetapi juga memberikan pengalaman pengguna yang optimal.</p>
+          <div className="flex items-center justify-between">
+            <img src={DataImage.HeroImage} alt="image" className="w-12 rounded-md sm:block hidden" />
+            <div className="flex items-center gap-6">
+              <div>
+                <h1 className="text-4xl mb-1">45<span className="text-violet-500">+</span></h1>
+                <p>Proyek Selesai</p>
+              </div>
+              <div>
+                <h1 className="text-4xl mb-1">4<span className="text-violet-500">+</span></h1>
+                <p>Tahun Pengalaman</p>
+              </div>
+            </div>
+          </div>
+      </div>
+      <div className="tools mt-32">
+        <h1 className="text-4xl/snug font-bold mb-4">Tools yang di pakai</h1>
+        <p className="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:3/4 w-full text-base/loose opacity-50">Berikut ini beberapa tools yang biasa saya pakai untuk pembuatan website</p>
+        <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+          {listTools.map(tool => (
+            <div className="flex items-center gap-2 p-3 border border-zinc-600 rounded-md hover:bg-zinc-800 group" key={tool.id}>
+            <img src={tool.gambar} alt="tools image" className="w-14 bg-zinc-800 p-1 group-hover:bg-zinc-900" />
+            <div>
+              <h4 className="font-bold">{tool.nama}</h4>
+              <p className="opacity-50">{tool.ket}</p>
+            </div>
+          </div>
+          ))}
+
+          
+        </div>
+      </div>
     </div>
     </>
   )
